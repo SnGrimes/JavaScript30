059cc014f4d5bbad3563058008c3e7a3e98d0f46
Shantia Grimes <shantiagrimes@gmail.com>
2018-05-20 11:56:05 -0500

    changed drum buttons to svg drum kit

--- 01 - JavaScript Drum Kit/JS/script.js
+++ 01 - JavaScript Drum Kit/JS/script.js
@@ -2,58 +2,70 @@
 
 
 
-function playAudio(event) {
-    var boom = new Audio('sounds/boom.wav');
-    var clap = new Audio('sounds/clap.wav');
-    var hihat = new Audio('sounds/hihat.wav');
-    var kick = new Audio('sounds/kick.wav');
-    var openhat = new Audio('sounds/openhat.wav');
-    var ride = new Audio('sounds/ride.wav');
-    var snare = new Audio('sounds/snare.wav');
-    var tink = new Audio('sounds/tink.wav');
-    var tom = new Audio('sounds/tom.wav');
+// function playAudio(event) {
+//     var boom = new Audio('sounds/boom.wav');
+//     var clap = new Audio('sounds/clap.wav');
+//     var hihat = new Audio('sounds/hihat.wav');
+//     var kick = new Audio('sounds/kick.wav');
+//     var openhat = new Audio('sounds/openhat.wav');
+//     var ride = new Audio('sounds/ride.wav');
+//     var snare = new Audio('sounds/snare.wav');
+//     var tink = new Audio('sounds/tink.wav');
+//     var tom = new Audio('sounds/tom.wav');
 
-    function animation () {
-        let sound = document.getElementById(event.target.id);
-        sound.classList.toggle('play');
-        setTimeout(() => {
-            sound.classList.remove('play');
-        }, 2000);
+//     function animation () {
+//         let sound = document.getElementById(event.target.id);
+//         sound.classList.toggle('play');
+//         setTimeout(() => {
+//             sound.classList.remove('play');
+//         }, 2000);
 
+//     }
+
+//     console.log(event.target.id);   
+//     switch (event.target.id) {
+//         case 'boom':
+//             boom.play();
+//             animation();        
+//             break;
+//         case 'clap':
+//             clap.play();
+//             break;
+//         case 'hihat':
+//             hihat.play();
+//             break;
+//         case 'kick':
+//             kick.play();
+//             break;
+//         case 'openhat':
+//             openhat.play();
+//             break;
+//         case 'ride':
+//             ride.play();
+//             break;
+//         case 'snare':
+//             snare.play();
+//             break;
+//         case 'tink':
+//             tink.play();
+//             break;
+//         case 'tom':
+//             tom.play();
+//             break;   
+
+//     }      
+// }
+
+//var drumkit = document.getElementById("drumkit").addEventListener('click', playAudio, false);
+const ride = document.getElementById("ride");
+const cymbal = document.getElementById("ride_cymbal").addEventListener('click', function() {
+    ride.play();
+}, false);
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case '65':
+        ride.play();
+        break;
     }
+});
 
-    console.log(event.target.id);   
-    switch (event.target.id) {
-        case 'boom':
-            boom.play();
-            animation();        
-            break;
-        case 'clap':
-            clap.play();
-            break;
-        case 'hihat':
-            hihat.play();
-            break;
-        case 'kick':
-            kick.play();
-            break;
-        case 'openhat':
-            openhat.play();
-            break;
-        case 'ride':
-            ride.play();
-            break;
-        case 'snare':
-            snare.play();
-            break;
-        case 'tink':
-            tink.play();
-            break;
-        case 'tom':
-            tom.play();
-            break;   
-
-    }      
-}
-
-var drumkit = document.getElementById("drumkit").addEventListener('click', playAudio, false);
